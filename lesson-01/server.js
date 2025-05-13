68d1dadcc9947050d904de0e7a7319e10a6c3be9
Sarah Moradi <moradisarah2001@gmail.com>
2025-05-13 18:22:31 +0330

    feat(mvc): add model and controller

--- lesson-01/server.js
+++ lesson-01/server.js
@@ -1,13 +1,12 @@
 const http = require("http");
 const PORT = 3000;
 const products = require("./data/products.json");
+const { ProductController } = require("./controller/product.controllers");
 
 const server = http.createServer((req, res) => {
   const { url } = req;
   if (url === "/api/products") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(products));
-    res.end();
+    ProductController.get(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.write(
