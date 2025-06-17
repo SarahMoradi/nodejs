5289d810d86f828ba7c72109d3b69bc7b1f7049d
sarah <moradisarah2001@gmail.com>
2025-06-17 20:40:11 +0330

    feat(db): connect mongodb

---
+++ mongodb/server.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb")
+
+const DB_url = "mongodb://localhost:27017"
+const DB_name = "mongodb_nodejs"
+
+const client = new MongoClient(DB_url)
+
+async function connect() {
+  try {
+    await client.connect()
+    const db = client.db(DB_name)
+    const userCollection = db.collection("user")
+
+    const users = await userCollection.find({}).toArray()
+    console.log(users)
+
+    console.log("✅ Connected to MongoDB")
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error)
+  } finally {
+    await client.close()
+  }
+}
+
+connect()
