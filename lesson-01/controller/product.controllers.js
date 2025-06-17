6aaa32b68a0cf320f97b27b9f943244f65621bbd
sarah <moradisarah2001@gmail.com>
2025-06-17 03:49:10 +0330

    feat(product): create product with file system

--- lesson-01/controller/product.controllers.js
+++ lesson-01/controller/product.controllers.js
@@ -1,54 +1,58 @@
-const ProductModel = require("../model/product.model");
+const ProductModel = require('../model/product.model')
 
 async function get(req, res) {
   try {
-    const products = await ProductModel.find();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(products));
-    res.end();
+    const products = await ProductModel.find()
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.write(JSON.stringify(products))
+    res.end()
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 async function getById(req, res) {
   try {
-    const [, , , id] = req.url.split("/");
-    const product = await ProductModel.findById(id);
+    const [, , , id] = req.url.split('/')
+    const product = await ProductModel.findById(id)
     if (!product) {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.write(JSON.stringify({ message: "not found!" }));
-      res.end();
+      res.writeHead(404, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify({message: 'not found!'}))
+      res.end()
     } else {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(JSON.stringify(product));
-      res.end();
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify(product))
+      res.end()
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 async function createProduct(req, res) {
   try {
-    await ProductModel.create({
-      id: Date.now(),
-      name: "Pencil",
-      price: 3.4,
-    });
-    res.writeHead(201, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "product created successfully!" }));
-    res.end();
+    let body = ''
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+    req.on('end', async () => {
+      const product = {id: Date.now(), ...JSON.parse(body)}
+      // Validate product data here if needed
+      const result = await ProductModel.create(product)
+      res.writeHead(201, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify(result))
+      res.end()
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 const ProductController = {
   get,
   getById,
-  createProduct
-};
+  createProduct,
+}
 
 module.exports = {
   ProductController,
-};
+}
--- lesson-01/model/product.model.js
+++ lesson-01/model/product.model.js
@@ -1,28 +1,36 @@
-const products = require("./../data/products.json");
+const products = require('./../data/products.json')
+const fs = require('fs')
 
 async function find() {
   return new Promise((resolve, reject) => {
-    resolve(products);
-  });
+    resolve(products)
+  })
 }
 
 async function findById(id) {
   return new Promise((resolve, reject) => {
-    resolve(products.find((product) => product.id == id));
-  });
+    resolve(products.find((product) => product.id == id))
+  })
 }
 
 async function create(product) {
   return new Promise((resolve, reject) => {
-    products.push(product);
-    resolve();
-  });
+    products.push(product)
+    fs.writeFile(`${process.cwd()}/data/products.json`, JSON.stringify(products), (err) => {
+      if (err) {
+        console.error('Error writing to file', err)
+        return reject(err)
+      } else {
+        resolve(product)
+      }
+    })
+  })
 }
 
 const ProductModel = {
   find,
   findById,
   create,
-};
+}
 
-module.exports = ProductModel;
+module.exports = ProductModel
